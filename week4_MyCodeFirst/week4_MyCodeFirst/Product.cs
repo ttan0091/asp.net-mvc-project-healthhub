0255d2e221c7377d8b29ec6bdafaa5e34c0d1ec8
Tao Tan <126834441+ttan0091@users.noreply.github.com>
2023-08-11 16:12:35 +1000

    create new MyCodeFirst file

---
+++ week4_MyCodeFirst/week4_MyCodeFirst/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace week4_MyCodeFirst
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
