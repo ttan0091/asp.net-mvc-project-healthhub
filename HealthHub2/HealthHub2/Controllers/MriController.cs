d4c645683e3baa1cd3227d7f291af2598f1dcd17
Tao Tan <126834441+ttan0091@users.noreply.github.com>
2023-08-28 23:08:57 +1000

    add map api

--- HealthHub2/HealthHub2/Controllers/MriController.cs
+++ HealthHub2/HealthHub2/Controllers/MriController.cs
@@ -9,9 +9,45 @@ namespace HealthHub2.Controllers
     public class MriController : Controller
     {
         // GET: Mri
-        public ActionResult Index()
+        public ActionResult Index(string viewName)
         {
+            ViewBag.ViewName = viewName ?? "BookMriService"; // 默认为 "BookMriService"
             return View();
+        }
+
+        public ActionResult BookMriService()
+        {
+            return PartialView();
+        }
+
+        public ActionResult BookConsultation()
+        {
+            return PartialView();
+        }
+
+        public ActionResult CheckImages()
+        {
+            return PartialView();
+        }
+
+        public ActionResult CheckConsultation()
+        {
+            return PartialView();
+        }
+
+        public ActionResult CheckBills()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SendEmail()
+        {
+            return PartialView();
+        }
+
+        public ActionResult RateDoctor()
+        {
+            return PartialView();
         }
     }
 }
