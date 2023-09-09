8e365c592cb24dd743ade1b28aff81b6c6f493f9
Tao Tan <126834441+ttan0091@users.noreply.github.com>
2023-09-09 16:28:14 +1000

    prepare to rewrite the project

---
+++ week7/week7/Controllers/UnitsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using week7.Models;
+
+namespace week7.Controllers
+{
+    public class UnitsController : Controller
+    {
+        private FIT5032_Models db = new FIT5032_Models();
+
+        // GET: Units
+        public ActionResult Index()
+        {
+            var units = db.Units.Include(u => u.Students);
+            return View(units.ToList());
+        }
+
+        // GET: Units/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Units units = db.Units.Find(id);
+            if (units == null)
+            {
+                return HttpNotFound();
+            }
+            return View(units);
+        }
+
+        // GET: Units/Create
+        public ActionResult Create()
+        {
+            ViewBag.StudentId = new SelectList(db.Students, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Units/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,StudentId")] Units units)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Units.Add(units);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StudentId = new SelectList(db.Students, "Id", "FirstName", units.StudentId);
+            return View(units);
+        }
+
+        // GET: Units/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Units units = db.Units.Find(id);
+            if (units == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StudentId = new SelectList(db.Students, "Id", "FirstName", units.StudentId);
+            return View(units);
+        }
+
+        // POST: Units/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,StudentId")] Units units)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(units).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StudentId = new SelectList(db.Students, "Id", "FirstName", units.StudentId);
+            return View(units);
+        }
+
+        // GET: Units/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Units units = db.Units.Find(id);
+            if (units == null)
+            {
+                return HttpNotFound();
+            }
+            return View(units);
+        }
+
+        // POST: Units/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Units units = db.Units.Find(id);
+            db.Units.Remove(units);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
