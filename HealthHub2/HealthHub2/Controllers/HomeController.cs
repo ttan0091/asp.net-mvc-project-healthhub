1cf72c0ebef3e2fd60df771d4ec8d537ec1a7eeb
Tao Tan <126834441+ttan0091@users.noreply.github.com>
2023-09-12 17:54:11 +1000

    prepare to add email function

--- HealthHub2/HealthHub2/Controllers/HomeController.cs
+++ HealthHub2/HealthHub2/Controllers/HomeController.cs
@@ -14,18 +14,18 @@ namespace HealthHub2.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
     }
 }
